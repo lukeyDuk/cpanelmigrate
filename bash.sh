93a90ab30e41f7f28cd765f995653df879ebfd36
lukeyDuk <87945598+lukeyDuk@users.noreply.github.com>
2023-01-26 18:06:52 +0000

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,62 @@
+# STILL TO DO
+# AUTO LOCATE THE DUMP FILE - LOCATION STANDARD?
+# EASILY SELECT CPANEL ACCOUNT WITHOUT MANUAL INPUT (IF POSSIBLE)
+# PULL DBNAME AND PASSWORD FROM THE WP-CONFIG (OR OTHER RELEVANT FILE)
+# LOG ERRORS SO WE CAN FIND FAILURES DURING MIGRATIONS 100K SITES
+# ADJUST WEBSITE PHP VERSION TO 4.4
+
+#!/bin/sh
+
+ echo "What is the cPanel username!"
+ read cpuser
+
+ cd /home/{cpuser}/public_html
+
+# Locate the relevant config (in this case wp-config)
+
+$wpconf = "find . -name wp-config.php -type f"
+
+$dbname = grep "DB_NAME" $wpconf
+
+$username = grep "DB_USER" $wpconf
+
+$userpass = grep "DB_PASSWORD" $wpconf
+
+$sqldump = "find . -name \*.sql -type f"
+
+# Create the db
+# If /root/.my.cnf exists then it won't ask for root password
+
+if [ -f /root/.my.cnf ]; then
+
+	   
+	echo "Creating new MySQL database..."
+	mysql -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+	echo "Database successfully created!"
+	
+    
+	echo "Creating new user..."
+	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
+	echo "User successfully created!"
+
+	echo "Granting ALL privileges on ${dbname} to ${username}!"
+	mysql -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';"
+	mysql -e "FLUSH PRIVILEGES;"
+	echo "You're good now on that, just importing the dump"
+
+    mysql -e ${dbname} < {$sqldump}
+
+    echo "Awesome, all done!"
+
+	exit
+
+#Map the DB to a user
+
+    echo "What is the cPanel username!"
+    read cpuser
+
+    /usr/local/cpanel/bin/dbmaptool ${cpuser} --type mysql --dbs ${dbname}
+
+    echo "We've added ${dbname} to the dbmap for account ${cpuser}"
+
+#ADVISE OF ERRORS
